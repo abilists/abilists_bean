44cf6cdfa54f4da1d0e77b02535b2495c62ea802
kim joon <minziappa@gmail.com>
2017-07-27 22:55:12 +0900

    Add to search function

---
+++ src/main/java/com/abilists/bean/para/profile/SrhUserSkillsPara.java
@@ -0,0 +1,44 @@
+package com.abilists.bean.para.profile;
+
+import com.abilists.bean.para.CommonPara;
+
+public class SrhUserSkillsPara extends CommonPara {
+
+	private String userId;
+	private String userName;
+	private String upIndustrial;
+	private String usKind;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUpIndustrial() {
+		return upIndustrial;
+	}
+
+	public void setUpIndustrial(String upIndustrial) {
+		this.upIndustrial = upIndustrial;
+	}
+
+	public String getUsKind() {
+		return usKind;
+	}
+
+	public void setUsKind(String usKind) {
+		this.usKind = usKind;
+	}
+
+}

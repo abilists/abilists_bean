d191499967f089d57b55bbafcdf229f6b34b480c
kim joon <minziappa@gmail.com>
2017-08-05 16:15:15 +0900

    Basic Model

--- src/main/java/com/abilists/bean/model/user/UserSkillsModel.java
+++ src/main/java/com/abilists/bean/model/user/UserSkillsModel.java
@@ -3,7 +3,9 @@ package com.abilists.bean.model.user;
 import java.io.Serializable;
 import java.util.Date;
 
-public class UserSkillsModel implements Serializable {
+import com.abilists.bean.model.BasicModel;
+
+public class UserSkillsModel extends BasicModel implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 

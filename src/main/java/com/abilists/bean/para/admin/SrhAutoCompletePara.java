fee4c7932cff0ba217066faf0bd37e0f98a999b1
kim joon <minziappa@gmail.com>
2018-02-18 12:12:07 +0900

    For auto complete

---
+++ src/main/java/com/abilists/bean/para/admin/SrhAutoCompletePara.java
@@ -0,0 +1,23 @@
+package com.abilists.bean.para.admin;
+
+import com.abilists.bean.para.CommonPara;
+
+public class SrhAutoCompletePara  extends CommonPara {
+
+	private String srhColumn;
+	private String srhContents;
+
+	public String getSrhColumn() {
+		return srhColumn;
+	}
+	public void setSrhColumn(String srhColumn) {
+		this.srhColumn = srhColumn;
+	}
+	public String getSrhContents() {
+		return srhContents;
+	}
+	public void setSrhContents(String srhContents) {
+		this.srhContents = srhContents;
+	}
+
+}

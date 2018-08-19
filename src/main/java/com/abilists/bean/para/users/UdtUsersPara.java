fff28e85288993ef0ad77af90efccb2e54d2ae19
kim joon <minziappa@gmail.com>
2018-08-19 15:01:57 +0900

    Fixed bugs

--- src/main/java/com/abilists/bean/para/users/UdtUsersPara.java
+++ src/main/java/com/abilists/bean/para/users/UdtUsersPara.java
@@ -11,7 +11,7 @@ public class UdtUsersPara extends CommonPara {
 	private String userDelete;
 
 	// @CharacterEscape(message = "parameter.error.escape.character.message")
-	@Size(min = 1, max = 1000, message = "parameter.error.size.max1000.message")
+	@Size(min = 1, max = 2500, message = "parameter.error.size.max2500.message")
 	private String userSummary;
 	
 	public String getUserAuth() {

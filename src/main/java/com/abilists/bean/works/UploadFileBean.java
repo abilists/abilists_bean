240bd466520e7e3238ada364628c755858b277ca
kim joon <minziappa@gmail.com>
2019-02-11 03:08:05 +0900

    For fixing bugs

---
+++ src/main/java/com/abilists/bean/works/UploadFileBean.java
@@ -0,0 +1,22 @@
+package com.abilists.bean.works;
+
+public class UploadFileBean {
+
+	private String ufNo;
+	private String ufExt;
+
+	public String getUfNo() {
+		return ufNo;
+	}
+	public void setUfNo(String ufNo) {
+		this.ufNo = ufNo;
+	}
+	public String getUfExt() {
+		return ufExt;
+	}
+	public void setUfExt(String ufExt) {
+		this.ufExt = ufExt;
+	}
+
+	
+}

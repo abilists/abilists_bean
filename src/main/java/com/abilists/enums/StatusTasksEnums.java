61d6bc2e69e9f438591815e4ddb302c26cb36c8f
njoonk <njoonk@njoonk-desktop>
2021-06-20 01:12:29 +0900

    Add a list on Enums

--- src/main/java/com/abilists/enums/StatusTasksEnums.java
+++ src/main/java/com/abilists/enums/StatusTasksEnums.java
@@ -1,12 +1,17 @@
 package com.abilists.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatusTasksEnums {
 
 	public enum TasksStatus {
 		NONE("None", "0"),
 	    PROGRESSING("Progressing", "1"),
 	    HOLDING("Holding", "2"),
-	    COMPLETED("Completed", "3");
+	    CANCEL("Cancel", "3"),
+//	    DROP("Drop", "4"),
+	    COMPLETED("Completed", "7");
 
 	    private String name;
 	    private String status;
@@ -23,6 +28,19 @@ public class StatusTasksEnums {
 	        return status;
 	    }
 
+	}
+
+	public static List<String> getValueList() {
+		List<String> allList = new ArrayList<>();
+
+		allList.add(TasksStatus.NONE.getStatus());
+		allList.add(TasksStatus.PROGRESSING.getStatus());
+		allList.add(TasksStatus.HOLDING.getStatus());
+		allList.add(TasksStatus.CANCEL.getStatus());
+//		allList.add(TasksStatus.DROP.getStatus()));
+		allList.add(TasksStatus.COMPLETED.getStatus());
+
+		return allList;
 	}
 
 }

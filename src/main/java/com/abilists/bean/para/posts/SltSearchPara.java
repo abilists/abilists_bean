a0a38aebbd83bab25a84dead4fe2b24c1aeb477c
abilists <abilists@gmail.com>
2024-10-12 02:16:52 +0900

    Add SltSearchPara

---
+++ src/main/java/com/abilists/bean/para/posts/SltSearchPara.java
@@ -0,0 +1,35 @@
+package com.abilists.bean.para.posts;
+
+public class SltSearchPara extends SltPostsPara {
+
+	private String upsTitle;
+	private String upsPost;
+	private String ucpTitle;
+	private String ucpComment;
+
+	public String getUpsTitle() {
+		return upsTitle;
+	}
+	public void setUpsTitle(String upsTitle) {
+		this.upsTitle = upsTitle;
+	}
+	public String getUpsPost() {
+		return upsPost;
+	}
+	public void setUpsPost(String upsPost) {
+		this.upsPost = upsPost;
+	}
+	public String getUcpTitle() {
+		return ucpTitle;
+	}
+	public void setUcpTitle(String ucpTitle) {
+		this.ucpTitle = ucpTitle;
+	}
+	public String getUcpComment() {
+		return ucpComment;
+	}
+	public void setUcpComment(String ucpComment) {
+		this.ucpComment = ucpComment;
+	}
+
+}
